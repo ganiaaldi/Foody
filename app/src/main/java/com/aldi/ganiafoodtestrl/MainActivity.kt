a47ad964d49b80dd53d7ed5536805942691903da
ganiaaldi <mrgad98@gmail.com>
2021-06-28 15:51:31 +0700

    implement menu & user interface activity_main.xml

--- app/src/main/java/com/aldi/ganiafoodtestrl/MainActivity.kt
+++ app/src/main/java/com/aldi/ganiafoodtestrl/MainActivity.kt
@@ -1,15 +1,61 @@
 package com.aldi.ganiafoodtestrl
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import com.aldi.ganiafoodtestrl.databinding.ActivityMainBinding
+import com.aldi.ganiafoodtestrl.ext.PrefManager
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var prefManager: PrefManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        prefManager = PrefManager(this)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.item_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.back_to_splash -> {
+                prefManager!!.setLaunched(true)
+                startActivity(Intent(this,SlideScreen::class.java))
+                true
+            }
+            R.id.credits -> {
+                showMessageBox("Credits")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun showMessageBox(text: String){
+
+        //Inflate the dialog as custom view
+        val messageBoxView = LayoutInflater.from(this).inflate(R.layout.layout_dialog, null)
+        //AlertDialogBuilder
+        val messageBoxBuilder = AlertDialog.Builder(this).setView(messageBoxView)
+        //show dialog
+        val  messageBoxInstance = messageBoxBuilder.show()
+        //set Listener
+        messageBoxView.setOnClickListener(){
+            //close dialog
+            messageBoxInstance.dismiss()
+        }
     }
 }
